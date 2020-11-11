96c5cb3cf5b2866683cca1bfac5c8f92e0620f5f
Adil Naimi <adilnm23@gmail.com>
2020-11-10 23:36:09 -0600

    Create destory action

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,17 +1,23 @@
 class PostsController < ApplicationController
     def index
         posts=Post.all
-        render json:@posts
+        render json:posts
     end
 
     def create
         post=Post.new(post_params)
-        
+
         if post.save
             render json: post
         else
             render json:{error:'not valid'}
         end
+    end
+
+    def destroy
+        post=Post.find(params[:id])
+        post.destroy
+        render json: {id:post.id}
     end
 
     private
